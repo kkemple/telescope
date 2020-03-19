112d9123c65a2990a31d7a2285135326e4170aa5
Kurt Kemple <kurtkemple@kurtsapollombp.lan>
2020-03-19 14:10:37 -0400

    type issues

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
-import './env';
+// globals
 import 'isomorphic-fetch';
-import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
-import { parseISO, differenceInDays, formatDistance, format } from 'date-fns';
+import './env';
+
+import outdent from 'outdent';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  gql,
+  NormalizedCacheObject,
+} from '@apollo/client';
+import { format } from 'date-fns';
+
+import generateReportForRepository from './report-generator';
 
 // Make into real types
 type GitHubResponse = any;
 
 const QUERY = gql`
-  query GitHubData($activeSince: String) {
+  query GitHubData($activeSince: string) {
     organization(login: "apollographql") {
       apolloTooling: repository(name: "apollo-tooling") {
         name
@@ -193,7 +204,7 @@ const QUERY = gql`
   }
 `;
 
-const client = new ApolloClient({
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
     uri: 'https://api.github.com/graphql',
@@ -202,162 +213,6 @@ const client = new ApolloClient({
     },
   }),
 });
-
-const generateTopTenStaleIssuesText = (staleIssues: any) => {
-  const staleIssuesText: string = staleIssues.nodes.reduce(
-    (mem: string, val: any) => {
-      const titleWithLink = `[${val.title}](${val.url})`;
-      const truncatedBody = `${val.bodyText.substr(0, 140)}`;
-
-      return `${mem}${titleWithLink}<br />${truncatedBody}...<br /><br />`;
-    },
-    ''
-  );
-
-  return `
-### Top Stale Issues
-${staleIssuesText}
-`;
-};
-
-const generateTopTenActiveIssuesText = (activeIssues: any) => {
-  const activeIssuesText: string = activeIssues.nodes.reduce(
-    (mem: string, val: any) => {
-      const titleWithLink = `[${val.title}](${val.url})`;
-      const truncatedBody = `${val.bodyText.substr(0, 140)}`;
-      const updatedAt = `${formatDistance(
-        parseISO(val.updatedAt),
-        new Date()
-      )}`;
-      const totalCommentCount = val.comments.totalCount
-        ? val.comments.totalCount
-        : 'no';
-      const totalReactionCount = val.comments.totalCount
-        ? val.comments.totalCount
-        : 'no';
-      const details = `There are ${totalCommentCount} comments on this issue, and it has ${totalReactionCount} reactions. It was last updated ${updatedAt} ago.`;
-
-      return `${mem}${titleWithLink}<br />${details}<br />${truncatedBody}...<br /><br />`;
-    },
-    ''
-  );
-
-  return `
-### Top Active Issues
-${activeIssuesText}
-`;
-};
-
-const generateAvgTimeToCloseIssuesText = (closedIssues: any) => {
-  const totalDaysBetweenOpenandClose: number = closedIssues.nodes.reduce(
-    (mem: number, val: any) => {
-      return (
-        mem + differenceInDays(parseISO(val.closedAt), parseISO(val.createdAt))
-      );
-    },
-    0
-  );
-
-  const avg = totalDaysBetweenOpenandClose / closedIssues.nodes.length;
-
-  return `On average it takes about ${Math.round(
-    avg
-  )} day(s) to close an issue.`;
-};
-
-const generateAvgTimeSinceLastUpdateText = (openIssues: any) => {
-  const totalDaysSinceLastUpdate: number = openIssues.nodes.reduce(
-    (mem: number, val: any) => {
-      const createdAt = parseISO(val.createdAt);
-      const updatedAt = parseISO(val.updatedAt);
-      const diff = differenceInDays(updatedAt, createdAt);
-
-      return mem + diff;
-    },
-    0
-  );
-
-  const avg = totalDaysSinceLastUpdate / openIssues.nodes.length;
-
-  return `On average issues have not been updated in about ${Math.round(
-    avg
-  )} day(s).`;
-};
-
-const generateAvgTimeToRespondToIssuesText = (openIssues: any) => {
-  const issuesWithoutComments: any = [];
-  const issuesWithComments: any[] = [];
-
-  openIssues.nodes.map((node: any) => {
-    if (node.comments.totalCount) {
-      issuesWithComments.push(node);
-    } else {
-      issuesWithoutComments.push(node);
-    }
-  });
-
-  const totalDaysBetweenFirstResponse: number = issuesWithComments.reduce(
-    (mem: number, val: any) => {
-      return (
-        mem +
-        differenceInDays(
-          parseISO(val.comments.nodes[0].createdAt),
-          parseISO(val.createdAt)
-        )
-      );
-    },
-    0
-  );
-
-  const avg = totalDaysBetweenFirstResponse / issuesWithComments.length;
-
-  return `On average it takes about ${Math.round(
-    avg
-  )} day(s) to respond to an issue. There are currently ${
-    issuesWithoutComments.length
-  } issues out of the last 100 that have not been responded to.`;
-};
-
-const generateReportForRepository = (repository: any): string => {
-  const numberOfOpenIssues: string = `There are ${repository.openIssuesForStats.totalCount} open issues currently.`;
-
-  const avgTimeToCloseIssues: string = generateAvgTimeToCloseIssuesText(
-    repository.closedIssuesForStats
-  );
-
-  const avgTimeToRespondToIssues: string = generateAvgTimeToRespondToIssuesText(
-    repository.openIssuesForStats
-  );
-
-  const avgTimeSinceLastUpdate: string = generateAvgTimeSinceLastUpdateText(
-    repository.openIssuesForStats
-  );
-
-  const topTenStaleIssues: string = generateTopTenStaleIssuesText(
-    repository.topTenStaleIssues
-  );
-
-  const topTenActiveIssues: string = generateTopTenActiveIssuesText(
-    repository.topTenActiveIssues
-  );
-
-  const title = `## ${repository.name}`;
-
-  const report: string = `
-${title}
-
-- ${numberOfOpenIssues}
-- ${avgTimeToRespondToIssues}
-- ${avgTimeSinceLastUpdate}
-- ${avgTimeToCloseIssues}
-
-${topTenActiveIssues}
-
-${topTenStaleIssues}
-`;
-
-  return report;
-};
 
 async function main(): Promise<void> {
   try {
@@ -374,24 +229,24 @@ async function main(): Promise<void> {
      * Avg time between review and merge
      */
 
-    const reportTitle = `Health Report for Apollo OSS Repositories - ${format(
+    const reportTitle: string = `Health Report for Apollo OSS Repositories - ${format(
       new Date(),
       'MMMM do, yyyy'
     )}`;
 
-    const toolingReport = generateReportForRepository(apolloTooling);
-    const clientReport = generateReportForRepository(apolloClient);
-    const serverReport = generateReportForRepository(apolloServer);
+    const toolingReport: string = generateReportForRepository(apolloTooling);
+    const clientReport: string = generateReportForRepository(apolloClient);
+    const serverReport: string = generateReportForRepository(apolloServer);
 
-    const aggregatedReport = `
-# ${reportTitle}
+    const aggregatedReport = outdent`
+      # ${reportTitle}
 
-${clientReport}
+      ${clientReport}
 
-${serverReport}
+      ${serverReport}
 
-${toolingReport}
-`;
+      ${toolingReport}
+    `;
 
     if (process.env.ZAPIER_WEBHOOK_URL) {
       fetch(process.env.ZAPIER_WEBHOOK_URL, {
